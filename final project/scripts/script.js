4077a495bfc265ba10a8efab176283b13d334d39
Miles <mcalabresi1@gmail.com>
2013-11-17 22:29:35 -0500

    added endgame; game works/is playable

--- final project/scripts/script.js
+++ final project/scripts/script.js
@@ -1,11 +1,12 @@
+// Based on tic-tac-toe by Alex Reinking
 String.prototype.replaceAt = function(index, character) {
     return this.substr(0, index) + character + this.substr(index+character.length);
 }
 
 var turn = 0;
-var gameover = false;
 var lastCellClicked = -1;
 var specturn = true;
+var squareswon = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
 // set up blank board
 var board  = "";
@@ -25,21 +26,43 @@ if (sym === 'O')
 	players = ['O', 'X'];
 
 
-// Define game over protocol
-if(gameover) {
-    alert(data.winner + ": " + data.message);
-    board = "";
-    for (var i = 0; i < 9; i ++)
-    	board += "         ";
-}
-
-
+// function to show the updated board on the HTML
 function renderBoard() {
     $("#turn").html("Player "+(1+turn%2)+"'s turn");
     for(var i = 0; i < 81; i++) {
         $("#subcell"+i).html(board[i]);
     }
 }
+
+// functions to check if a square has been won ()
+// start must be in column 1
+function rowWon(start, board) {
+    return (board[start] !== " " && board[start] === board[start+1] && board[start] === board[start+2]);
+}
+
+// start must be in row 1
+function colWon(start, board) {
+    return (board[start] !== " " && board[start] === board[start+3] && board[start] === board[start+6]);
+}
+
+// start must be upper left square
+function diagWon(start, board) {
+    if (board[start] !== " " && board[start] === board[start+4] && board[start] === board[start+8])
+        return true;
+    else
+        return (board[start+2] !== " " && board[start+2] === board[start+4] && board[start+2] === board[start+6]);
+}
+
+// start must be upper left square
+function squareWon(start, board) {
+    var res = false;
+    res = res || diagWon(start, board);
+    for (var i = 0; i < 3; i++)
+        res = res || (rowWon(start+3*i, board) || colWon(start+i, board));
+    return res;
+}
+
+
 
 // begin gameplay once the window loads
 $(function () {
@@ -52,21 +75,48 @@ $(function () {
         var subcellClicked = c[c.length-1];
     	if (c[c.length-2] !== 'l')
     		subcellClicked = c[c.length-2] + subcellClicked;
+        subcellClicked = Number(subcellClicked);
 
         // check that subcell was empty and replace
-        if (board[Number(subcellClicked)] === " " &&
+        if (board[subcellClicked] === " " &&
         (specturn || (9*(lastCellClicked%9) <= subcellClicked && subcellClicked < 9*((lastCellClicked%9)+1)))) {
-            console.log("turn "+turn+"; specturn: "+specturn);
-            board = board.replaceAt(Number(subcellClicked), players[turn%2]);
-            turn++;
-            lastCellClicked = subcellClicked;
+            board = board.replaceAt(subcellClicked, players[turn%2]);
 
-            // set special turn to be true for the special case when one sub-board is entirely full
-            //specturn = true;
+            renderBoard();
+            // see if this square has been won (it cannot have been won previously)
+            if (squareswon[Math.floor(subcellClicked/9)] === " " && squareWon(subcellClicked-(subcellClicked%9), board))
+                squareswon[Math.floor(subcellClicked/9)] = players[turn%2];
+
+            // see if the game has been won and reset/exit
+            if (squareWon(0, squareswon)) {
+                alert("Game over! Player "+(1+(turn%2))+" has won.");
+                if(confirm("Play again? 'OK' for yes, 'Cancel' for no")) {
+                    board = "";
+                    for (var i = 0; i < 9; i ++)
+                        board += "         ";
+                    turn = 0;
+                    lastCellClicked = -1;
+                    specturn = true;
+                    squareswon = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+                }
+                else
+                    close();
+            }
+
+
+            // set special turn to be true only for the special case when one sub-board is entirely full
+            specturn = true;
             for (var i = 9*(subcellClicked%9); i < 9*((subcellClicked%9)+1); i++) {
                 if (board[i] === " ")
                     specturn = false;
             }
+
+            // update variables to reflect this turn
+            turn++;
+            lastCellClicked = subcellClicked;
+            console.log("this turn: "+squareWon(subcellClicked-(subcellClicked%9), board));
+            console.log("squares claimed: ");
+            console.log(squareswon);
         }
 
         renderBoard();
@@ -74,5 +124,3 @@ $(function () {
 
 
 });
-
-
